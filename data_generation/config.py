9d4ec1a1712ff6eeaeca42a9f56842938bf664df
endrit_beqa <ge63qiz@mytum.de>
2024-07-28 20:41:50 +0200

    automatic paramater calculation for decoder

--- data_generation/config.py
+++ data_generation/config.py
@@ -6,11 +6,11 @@ def get_config():
     config = config_dict.ConfigDict()
     config.end_time = 500  # max number of simulation time steps
     config.purge_write = 1  # number of simulation timepoints to save
-    config.write_interval = 10  # interval to write simulation to disk
+    config.write_interval = 500  # interval to write simulation to disk
     config.save_timestep = [500]  # simulation timesteps to interpolate into arrays
-    config.res_params = [(32, [5, 300])]# res: [num_samples, simulation_timeout]
+    config.res_params = [(32, [4000, 300])]# res: [num_samples, simulation_timeout]
     config.validation_split = 0.2
-    config.num_workers = 5
+    config.num_workers = 4
     config.num_samples = 0  ## this will be set by the program from the num_samples_per_res dict
     config.res = 0  # this will be set by the program from the num_samples_per_res dict
     config.freestream_angle = math.pi / 8.  # -angle ... angle
@@ -19,7 +19,7 @@ def get_config():
     config.airfoil_database = "./airfoil_database/"
     config.output_dir = "./train/"
     config.clean_res_dir = True
-    config.save_images = True
+    config.save_images = False
     config.gmsh_timeout = 20
     config.gmshToFoam_timeout = 100
     config.simulation_timeout = 500
